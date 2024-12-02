876a305b297dfa812d292df4e509cad0c443c792
biancacpacheco <bianca.pacheco@ccc.ufcg.edu.br>
2024-12-02 14:39:06 -0300

    remove endpoints inutilizados

--- src/main/java/com/bookhive/ufcg/bookhive/controller/ReviewController.java
+++ src/main/java/com/bookhive/ufcg/bookhive/controller/ReviewController.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bookhive.ufcg.bookhive.dto.ReviewDTO;
 import com.bookhive.ufcg.bookhive.exception.BookNotFoundException;
 import com.bookhive.ufcg.bookhive.exception.ReviewConflictException;
-import com.bookhive.ufcg.bookhive.exception.ReviewNotFoundException;
 import com.bookhive.ufcg.bookhive.exception.UserNotFoundException;
 import com.bookhive.ufcg.bookhive.model.Review;
 import com.bookhive.ufcg.bookhive.service.ReviewService;
@@ -48,60 +45,10 @@ public class ReviewController {
         }
     }
 
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<Review> getReviewById(@PathVariable String id) {
-        try {
-            Review review = reviewService.getReviewById(id);
-            return new ResponseEntity<>(review, HttpStatus.OK);
-        } catch (ReviewNotFoundException e) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-    }
-
-    
     @RequestMapping(value = "reviews", method = RequestMethod.GET)
     public ResponseEntity<List<Review>> listReviews() {
         List<Review> reviews = reviewService.listReviews();
         return new ResponseEntity<>(reviews, HttpStatus.OK);
-    }
-
-    
-    @PutMapping("/{id}")
-    public ResponseEntity<String> updateReview(
-            @PathVariable String id,
-            @RequestBody ReviewDTO reviewDTO) {
-        try {
-            try {
-                reviewService.updateReview(
-                        id,
-                        reviewDTO.getUsernameUser(),
-                        reviewDTO.getBookIsbn(),
-                        reviewDTO.getStartDate(),
-                        reviewDTO.getEndDate(),
-                        reviewDTO.getRating(),
-                        reviewDTO.getContent()
-                );
-            } catch (UserNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (BookNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            return new ResponseEntity<>("Resenha atualizada com sucesso", HttpStatus.OK);
-        } catch (ReviewNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
-    }
-
-    
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteReview(@PathVariable String id) {
-        try {
-            reviewService.removeReview(id);
-            return new ResponseEntity<>("Resenha removida com sucesso", HttpStatus.OK);
-        } catch (ReviewNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
     }
 
     @GetMapping("/user/{username}")
